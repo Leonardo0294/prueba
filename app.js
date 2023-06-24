fd73475597ee52eed2e2793933a941e7c5c951aa
Leo9428 <127988282+Leo9428@users.noreply.github.com>
2023-06-24 19:43:51 -0300

    segundo commit

--- app.js
+++ app.js
@@ -17,4 +17,4 @@ app.use('/api', require('./routes/reserva.routes'));
 // TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
 
 // Starting the server
-app.listen(45635, () => console.log('Server on port xxxx'));
+app.listen(45635, () => console.log('Server on port 45635'));
